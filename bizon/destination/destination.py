ebbf1f42c544472e4b3d3adffbcd95e325978f98
Antoine Balliet <antoine.balliet@gorgias.com>
2024-12-21 00:43:46 +0100

    format

--- bizon/destination/destination.py
+++ bizon/destination/destination.py
@@ -248,22 +248,30 @@ class DestinationFactory:
     ) -> AbstractDestination:
 
         if config.name == DestinationTypes.LOGGER:
-            from bizon.connectors.destinations.logger.src.destination import LoggerDestination
+            from bizon.connectors.destinations.logger.src.destination import (
+                LoggerDestination,
+            )
 
             return LoggerDestination(sync_metadata=sync_metadata, config=config.config, backend=backend)
 
         elif config.name == DestinationTypes.BIGQUERY:
-            from bizon.connectors.destinations.bigquery.src.destination import BigQueryDestination
+            from bizon.connectors.destinations.bigquery.src.destination import (
+                BigQueryDestination,
+            )
 
             return BigQueryDestination(sync_metadata=sync_metadata, config=config.config, backend=backend)
 
         elif config.name == DestinationTypes.BIGQUERY_STREAMING:
-            from bizon.connectors.destinations.bigquery_streaming.src.destination import BigQueryStreamingDestination
+            from bizon.connectors.destinations.bigquery_streaming.src.destination import (
+                BigQueryStreamingDestination,
+            )
 
             return BigQueryStreamingDestination(sync_metadata=sync_metadata, config=config.config, backend=backend)
 
         elif config.name == DestinationTypes.FILE:
-            from bizon.connectors.destinations.file.src.destination import FileDestination
+            from bizon.connectors.destinations.file.src.destination import (
+                FileDestination,
+            )
 
             return FileDestination(sync_metadata=sync_metadata, config=config.config, backend=backend)
 
