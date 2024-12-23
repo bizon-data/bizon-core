46f6ef13ace0771607c1fccdfe57a51e7b6ba45d
Antoine Balliet <antoine.balliet@gorgias.com>
2024-12-23 10:57:00 +0100

    wip

--- bizon/engine/queue/adapters/rabbitmq/consumer.py
+++ bizon/engine/queue/adapters/rabbitmq/consumer.py
@@ -1,10 +1,15 @@
+import multiprocessing.synchronize
+import threading
+from typing import Union
+
 import pika
 import pika.connection
 from loguru import logger
 
 from bizon.destination.destination import AbstractDestination
-from bizon.engine.queue.config import QUEUE_TERMINATION
-from bizon.engine.queue.queue import AbstractQueueConsumer, QueueMessage
+from bizon.engine.pipeline.models import PipelineReturnStatus
+from bizon.engine.queue.config import QueueMessage
+from bizon.engine.queue.queue import AbstractQueueConsumer
 
 from .config import RabbitMQConfigDetails
 
@@ -14,35 +19,30 @@ class RabbitMQConsumer(AbstractQueueConsumer):
         super().__init__(config, destination=destination)
         self.config: RabbitMQConfigDetails = config
 
-    def run(self) -> None:
+    def run(self, stop_event: Union[threading.Event, multiprocessing.synchronize.Event]) -> None:
+
         connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.config.queue.host, port=self.config.queue.port)
+            pika.ConnectionParameters(
+                host=self.config.queue.host,
+                port=self.config.queue.port,
+            )
         )
 
         channel = connection.channel()
-
         channel.queue_declare(queue=self.config.queue.queue_name)
 
         for method_frame, properties, body in channel.consume(self.config.queue.queue_name):
 
-            queue_message = QueueMessage.model_validate_json(body)
-            if queue_message.signal == QUEUE_TERMINATION:
-                logger.info("Received termination signal, waiting for destination to close gracefully ...")
-                self.destination.write_records_and_update_cursor(
-                    source_records=queue_message.source_records,
-                    iteration=queue_message.iteration,
-                    extracted_at=queue_message.extracted_at,
-                    pagination=queue_message.pagination,
-                    last_iteration=True,
-                )
+            # Handle kill signal from the runner
+            if stop_event.is_set():
+                logger.info("Stop event is set, closing consumer ...")
+                return PipelineReturnStatus.KILLED_BY_RUNNER
+
+            queue_message = QueueMessage(**body)
+
+            status = self.process_queue_message(queue_message)
+
+            if status != PipelineReturnStatus.RUNNING:
                 channel.queue_delete(queue=self.config.queue.queue_name)
                 channel.close()
-                break
-
-            self.destination.write_records_and_update_cursor(
-                source_records=queue_message.source_records,
-                iteration=queue_message.iteration,
-                extracted_at=queue_message.extracted_at,
-                pagination=queue_message.pagination,
-            )
-            logger.info(f"Consumed data from queue: {len(queue_message.source_records)}")
+                return status
